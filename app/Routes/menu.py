c05b7c281f37de790c0ad80f1dabf848149bd5bd
whitleyo <owen.whitley@mail.utoronto.ca>
2025-04-12 18:19:00 -0400

    add simple menu

--- app/Routes/menu.py
+++ app/Routes/menu.py
@@ -1,3 +1,20 @@
 from flask import Blueprint, request, jsonify
 
 menu_bp = Blueprint('menu', __name__, url_prefix='/menu')
+
+# Menu items. Can be replaced with requests to SQL database in future.
+# menu_items = [
+#     {"id": 1, "name": "Espresso", "price": 3.00},
+#     {"id": 2, "name": "Latte", "price": 4.50},
+#     {"id": 3, "name": "Cappuccino", "price": 4.00}
+# ]
+
+@menu_bp.route('/', methods=['GET'])
+def get_menu():
+    """Retrieve the coffee menu."""
+    # return jsonify(menu_items), 200
+    # For now, we return a status code and a placeholder string
+    # in json format
+    status=0
+    data="data"
+    return jsonify({status: status, data: data})

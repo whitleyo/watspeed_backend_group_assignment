64a98a52631b46aa97c498d1e5cea2f91d8da303
whitleyo <owen.whitley@mail.utoronto.ca>
2025-04-13 20:34:05 -0400

    comment out call to flask_expects_json rerun unit tests

--- tests/unit/test_user_service.py
+++ tests/unit/test_user_service.py
@@ -1,5 +1,8 @@
 import pytest
 from flask import Flask
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 from app.Routes.auth import auth_bp
 
 @pytest.fixture

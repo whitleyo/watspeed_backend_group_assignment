812f6e572bed5083670606b2fee03c326e91fef4
samyang <yqf19971124@gmail.com>
2025-04-23 18:39:25 +0800

    remove unused domain models and DTOs for reservations and user authentication

--- messages/requests/auth_request_dto.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, EmailStr, Field
-
-# Originally this was separated into two DTOs, one for login and one for registration
-# but since they have the same fields, we can combine them into one
-class UserRequestDto(BaseModel):
-    """DTO for logging in or creating a new user."""
-    username: str = Field(..., description="User name")
-    email: EmailStr = Field(..., description="User email address")

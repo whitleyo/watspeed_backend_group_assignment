174058d2284c78b1218d5b10d2ac41fab795993b
browne89 <emilylynbrown@gmail.com>
2025-04-29 23:21:10 -0400

    injection for UserDAO

--- app/daos/__init__.py
+++ app/daos/__init__.py
@@ -1,8 +1,9 @@
 # Initialize DAO package
 from .table_dao import TableDAO
 from .reservation_dao import ReservationDAO
+from .user_dao import UserDAO
 #from .coffee_dao import CoffeeDAO
 #from .shop_dao import ShopDAO
 
 #__all__ = ['TableDAO', 'ReservationDAO', 'CoffeeDAO', 'ShopDAO']
-__all__ = ['TableDAO', 'ReservationDAO']
+__all__ = ['TableDAO', 'ReservationDAO', 'UserDAO']

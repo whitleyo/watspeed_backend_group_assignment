8d2b08f0943272e7fd39feb7dec709cd955e73b9
browne89 <emilylynbrown@gmail.com>
2025-05-04 12:26:11 -0400

    add html calculator template and run in app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from app import create_app
+from flask import render_template
 
 app = create_app()
+
+@app.route('/calculator')
+def calculator():
+    return render_template('calculator.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
